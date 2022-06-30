23419f09befa81808ed6085b29235204486b29ea
Cristian Benegas <benegascristian@gmail.com>
2022-06-30 18:50:10 -0300

    Correcciones en extensiones

--- src/components/CartWidget.js
+++
@@ -1,7 +0,0 @@
-import './style.css'
-
-const CartWidget = () => {
-    return (<h2 className="logo">Mi tienda</h2>)
-}
-
-export default CartWidget
--- src/components/ItemListContainer.js
+++
@@ -1,9 +0,0 @@
-import './style.css'
-
-const ItemListContainer = ({greeting}) => {
-    return (
-        <p className="greeting">¡Hola {greeting}!</p>
-    )
-}
-
-export default ItemListContainer
